d8a26619d3d5e8db67baec301b483b3b76c39e54
Abhiram <abhiram20001@gmail.com>
2022-02-17 00:22:51 +0530

    Bottom Navigation added (bugged)

--- app/src/main/java/com/shcollege/bfituser/MenuActivity.java
+++ app/src/main/java/com/shcollege/bfituser/MenuActivity.java
@@ -4,11 +4,17 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MenuActivity extends AppCompatActivity {
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class  MenuActivity extends AppCompatActivity {
+
+    BottomNavigationView bottomNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
+
+        bottomNavigationView = (BottomNavigationView) findViewById(R.id.bottomNavigationView);
     }
 }
